933446bf1389cf4b872d228a66b57db66e896193
Denis Dmitriev <der.flinger1397@gmail.com>
2023-01-31 12:55:16 +0300

    Solved Task 10

---
+++ HomeTask №1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// Console.WriteLine("Hello, World!");
+// Console.ReadLine("")
+
+Console.Clear();
+
+Console.Write("Enter our Number: ");        // Вводим наше число в виде строки и выводим второй элемент этой строки
+string num = Console.ReadLine();
+
+Console.WriteLine(num[1]);
