95f57de5f94c27240cd2bfd9b26ced31cfce9ffb
Denis Dmitriev <der.flinger1397@gmail.com>
2023-02-08 18:24:31 +0300

    Solved Task 27

---
+++ Hometask N2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+System.Console.Write("Вводимое число: ");
+int num = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine($"Сумма цифр цисла = {SummNumbers(num)}");
+
+int SummNumbers(int n)
+{
+    if (n < 0) n *= -1;
+    
+    int result = 0;
+    while (n > 0)
+    {
+        result += n % 10;
+        n /= 10;
+    }
+    return result;
+}
