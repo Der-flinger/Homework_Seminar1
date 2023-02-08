827ac7d2f2650cd28b0e8f6bd44e0c4944bbc2e8
Denis Dmitriev <der.flinger1397@gmail.com>
2023-02-08 18:10:09 +0300

    Solved Task 25

---
+++ Hometask N1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+System.Console.Write("Вводимое число А: ");
+int A = int.Parse(Console.ReadLine()!);
+
+System.Console.Write("Вводимая степень В: ");
+int B = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine($"А в степени В = {PowerAB(A, B)}");
+
+double PowerAB(double a, double b)
+{
+    double result = Math.Pow(a, b);
+    return result;
+}
