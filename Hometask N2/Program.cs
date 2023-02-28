5f603ddc8067942b2fb318bf93bcd88eec7eaf74
Denis Dmitriev <der.flinger1397@gmail.com>
2023-02-28 15:14:34 +0300

    Solved task 66

---
+++ Hometask N2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+System.Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+System.Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+if (M > N)                                  // если первое число больше второго, то программа это учтет
+{
+    int temp = M;
+    M = N;
+    N = temp;
+}
+int result = GetSumNumbers(M, N);
+System.Console.WriteLine($"Сумма натуральных чисел равна {result}");
+
+int GetSumNumbers(int num1, int num2)
+{
+    int sum = num2;
+    if (num2 < num1 || num2 <= 0) return 0;
+    return sum += GetSumNumbers(num1, num2 - 1);
+}
